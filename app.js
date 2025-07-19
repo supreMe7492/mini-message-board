1976af8bd70a62313a178217511293bea54a22bf
supreMe7492 <supremeraut94@gmail.com>
2025-07-19 14:06:08 +0545

    deployement settings

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.get("/new", (req, res) => {
 });
 const newRouter = newPost(messages);
 app.use("/new", newRouter);
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("works");
 });
